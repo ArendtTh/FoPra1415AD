f064a94a4daa213deb88c4486433e038fe902681
msg2015 <clown2000@gmx.de>
2015-03-31 11:54:03 +0200

    Number of Merge Nodes added

---
+++ de.unimarburg.swt.fopra.activity.smells/src/org/eclipse/emf/refactor/modelsmell/ATD.java
@@ -0,0 +1,62 @@
+package org.eclipse.emf.refactor.modelsmell;
+
+import java.util.LinkedList;
+
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.refactor.smells.interfaces.IModelSmellFinder;
+import org.eclipse.uml2.uml.Activity;
+import org.eclipse.uml2.uml.ActivityEdge;
+import org.eclipse.uml2.uml.DecisionNode;
+import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.MergeNode;
+import org.eclipse.uml2.uml.Model;
+import org.eclipse.uml2.uml.OpaqueAction;
+
+
+public final class ATD implements IModelSmellFinder {
+
+	@Override
+	public LinkedList<LinkedList<EObject>> findSmell(EObject root) {
+		LinkedList<LinkedList<EObject>> results = new LinkedList<LinkedList<EObject>>();
+		Activity model = (Activity) root;
+		System.out.println("vor schleifen passts");
+			EList<ActivityEdge> allActivityEdges = getAllActivityEdges(model);
+			for (ActivityEdge activityEdge : allActivityEdges) {
+				if ( (activityEdge.getSource() instanceof OpaqueAction)
+						&& (activityEdge.getTarget() instanceof DecisionNode) ) {
+					LinkedList<EObject> result = new LinkedList<EObject>();
+					result.add(activityEdge);
+					// TODO
+					results.add(result);
+				}
+			}
+		return results;
+	}
+
+	private EList<ActivityEdge> getAllActivityEdges(Activity activity) {
+		EList<Element> elems = activity.allOwnedElements();
+		EList<ActivityEdge> activityEdges = new BasicEList<ActivityEdge>();
+//		System.out.println("Hallo Welt, hier ist getAllActivityEdges: ");
+		for(Element elem : elems) {
+			if (elem instanceof ActivityEdge) {
+				activityEdges.add((ActivityEdge) elem);
+			}
+		}
+		return activityEdges;
+	}
+
+	private EList<Activity> getAllActivities(Model model) {
+		EList<Element> elems = model.allOwnedElements();
+		EList<Activity> activities = new BasicEList<Activity>();
+//		System.out.println("Hallo Welt, hier ist getAllActivityEdges: ");
+		for (Element elem : elems) {
+			if (elem instanceof Activity) {
+				activities.add((Activity) elem);
+			}
+		}
+		return activities;
+	}
+	
+}
--- de.unimarburg.swt.fopra.activity.smells/src/org/eclipse/emf/refactor/modelsmell/MergeTransitionDecision.java
+++ de.unimarburg.swt.fopra.activity.smells/src/org/eclipse/emf/refactor/modelsmell/MergeTransitionDecision.java
@@ -8,7 +8,9 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.refactor.smells.interfaces.IModelSmellFinder;
 import org.eclipse.uml2.uml.Activity;
 import org.eclipse.uml2.uml.ActivityEdge;
+import org.eclipse.uml2.uml.DecisionNode;
 import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.MergeNode;
 import org.eclipse.uml2.uml.Model;
 
 
@@ -17,31 +19,37 @@ public final class MergeTransitionDecision implements IModelSmellFinder {
 	@Override
 	public LinkedList<LinkedList<EObject>> findSmell(EObject root) {
 		LinkedList<LinkedList<EObject>> results = new LinkedList<LinkedList<EObject>>();
-		Model model = (Model) root;
-		EList<Activity> allActivities = getAllActivities(model);
-		for (Activity activity : allActivities) {
-			EList<ActivityEdge> allActivityEdges = getAllActivityEdges(activity);
+		Activity model = (Activity) root;
+		System.out.println("vor schleifen passts");
+			EList<ActivityEdge> allActivityEdges = getAllActivityEdges(model);
 			for (ActivityEdge activityEdge : allActivityEdges) {
-				// TODO
-				if (true) {
+				if ( (activityEdge.getSource() instanceof MergeNode)
+						&& (activityEdge.getTarget() instanceof DecisionNode) ) {
 					LinkedList<EObject> result = new LinkedList<EObject>();
 					result.add(activityEdge);
 					// TODO
 					results.add(result);
 				}
 			}
-		}
 		return results;
 	}
 
 	private EList<ActivityEdge> getAllActivityEdges(Activity activity) {
-		// TODO Auto-generated method stub
-		return null;
+		EList<Element> elems = activity.allOwnedElements();
+		EList<ActivityEdge> activityEdges = new BasicEList<ActivityEdge>();
+//		System.out.println("Hallo Welt, hier ist getAllActivityEdges: ");
+		for(Element elem : elems) {
+			if (elem instanceof ActivityEdge) {
+				activityEdges.add((ActivityEdge) elem);
+			}
+		}
+		return activityEdges;
 	}
 
 	private EList<Activity> getAllActivities(Model model) {
 		EList<Element> elems = model.allOwnedElements();
 		EList<Activity> activities = new BasicEList<Activity>();
+//		System.out.println("Hallo Welt, hier ist getAllActivityEdges: ");
 		for (Element elem : elems) {
 			if (elem instanceof Activity) {
 				activities.add((Activity) elem);
